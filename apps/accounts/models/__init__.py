6f900c89715bc3e32f6be7b578fad80cc23e76e6
André Dias <andremssdias@hotmail.com>
2022-02-18 18:45:04 +0100

    feat(accounts): Reorder folders

--- apps/accounts/models/__init__.py
+++ apps/accounts/models/__init__.py
@@ -1,3 +1,2 @@
 from apps.accounts.models.user import User
 from apps.accounts.models.profile import Profile
-from apps.accounts.models.signals import *
--- apps/accounts/models/signals/__init__.py
+++
@@ -1,1 +0,0 @@
-from .create_profile import *
