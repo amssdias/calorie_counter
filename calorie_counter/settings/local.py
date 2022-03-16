51e9460f1d8a8e5149bfe4c45381092edf10f0d6
André Dias <andremssdias@hotmail.com>
2022-03-16 21:04:37 +0100

    Create new settings tests for tests

--- calorie_counter/settings/local.py
+++ calorie_counter/settings/local.py
@@ -1,6 +1,19 @@
+import os
+
 SECRET_KEY="django-insecure-+01rjt^m#w9_jxu!s6##q3j6fch*i5_h#_x4rn0q!zni&3qmls"
 
 DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER", "user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", ""),
+    }
+}
 
 # Celery
 CELERY_ENABLED = False
@@ -15,55 +28,55 @@ SECURE_SSL_HOST = ""
 SECURE_SSL_REDIRECT = False
 
 # LOGS
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": True,
-#     "loggers": {
-#         "django": {
-#             "handlers": ["file_info", "file_warning", "file_error"],
-#             "level": "INFO",
-#         },
-#     },
-#     "handlers": {
-#         "console": {
-#             "level": "INFO",
-#             "class": "logging.StreamHandler",
-#             "formatter": "simple_format",
-#         },
-#         "file_info": {
-#             "level": "DEBUG",
-#             "class": "logging.FileHandler",
-#             "filename": "./logs/info.log",
-#             "formatter": "simple_format",
-#         },
-#         "file_warning": {
-#             "level": "WARNING",
-#             "class": "logging.FileHandler",
-#             "filename": "./logs/warning.log",
-#             "formatter": "warning_format",
-#         },
-#         "file_error": {
-#             "level": "ERROR",
-#             "class": "logging.FileHandler",
-#             "filename": "./logs/error.log",
-#             "formatter": "error_format",
-#         },
-#     },
-#     "formatters": {
-#         "simple_format": {
-#             "format": "{levelname:8s} {asctime}: {message} - {filename}: {lineno}",
-#             "style": "{",
-#             "datefmt": "%d-%m-%Y %H:%M:%S",
-#         },
-#         "warning_format": {
-#             "format": "{levelname:8s} {asctime}: {message} - {filename}: {lineno}",
-#             "style": "{",
-#             "datefmt": "%d-%m-%Y %H:%M:%S",
-#         },
-#         "error_format": {
-#             "format": "{levelname:8s}: {asctime} {filename} {message} - {pathname}: {lineno}",
-#             "style": "{",
-#             "datefmt": "%d-%m-%Y %H:%M:%S",
-#         },
-#     }
-# }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "loggers": {
+        "django": {
+            "handlers": ["file_info", "file_warning", "file_error"],
+            "level": "INFO",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "simple_format",
+        },
+        "file_info": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "./logs/info.log",
+            "formatter": "simple_format",
+        },
+        "file_warning": {
+            "level": "WARNING",
+            "class": "logging.FileHandler",
+            "filename": "./logs/warning.log",
+            "formatter": "warning_format",
+        },
+        "file_error": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": "./logs/error.log",
+            "formatter": "error_format",
+        },
+    },
+    "formatters": {
+        "simple_format": {
+            "format": "{levelname:8s} {asctime}: {message} - {filename}: {lineno}",
+            "style": "{",
+            "datefmt": "%d-%m-%Y %H:%M:%S",
+        },
+        "warning_format": {
+            "format": "{levelname:8s} {asctime}: {message} - {filename}: {lineno}",
+            "style": "{",
+            "datefmt": "%d-%m-%Y %H:%M:%S",
+        },
+        "error_format": {
+            "format": "{levelname:8s}: {asctime} {filename} {message} - {pathname}: {lineno}",
+            "style": "{",
+            "datefmt": "%d-%m-%Y %H:%M:%S",
+        },
+    }
+}
