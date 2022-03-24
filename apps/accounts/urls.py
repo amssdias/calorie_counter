5a2d533973e725d7fd138bf6b39428d6e516f346
André Dias <andremssdias@hotmail.com>
2022-03-24 01:16:25 +0100

    feat(accounts): black format

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,27 +4,41 @@ from django.views.generic.base import RedirectView
 from django.contrib.auth.views import LogoutView
 
 from .views import (
-    RegisterView, 
-    CustomLoginView, 
-    ActivateAccountView, 
-    CustomPasswordResetView, 
+    RegisterView,
+    CustomLoginView,
+    ActivateAccountView,
+    CustomPasswordResetView,
     CustomPasswordResetConfirmView,
-    ProfileDetailView, 
-    )
+    ProfileDetailView,
+)
 
 app_name = "accounts"
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", include([
-        path("", CustomLoginView.as_view(), name='login'),
-        path("activate_account/<str:uidb64>/<str:token>", ActivateAccountView.as_view(), name="activate_account"),
-    ])),
-
-    path("forgot_password/", CustomPasswordResetView.as_view(), name="reset_password_custom"),
-    path("reset/<uidb64>/<token>", CustomPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-
+    path(
+        "login/",
+        include(
+            [
+                path("", CustomLoginView.as_view(), name="login"),
+                path(
+                    "activate_account/<str:uidb64>/<str:token>",
+                    ActivateAccountView.as_view(),
+                    name="activate_account",
+                ),
+            ]
+        ),
+    ),
+    path(
+        "forgot_password/",
+        CustomPasswordResetView.as_view(),
+        name="reset_password_custom",
+    ),
+    path(
+        "reset/<uidb64>/<token>",
+        CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
     path("profile/<uuid:uuid>/", ProfileDetailView.as_view(), name="profile"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("", RedirectView.as_view(url="login")),
 ]
-
