d286797056286b5c3ece499046cfcf1486b7a4cd
André Dias <andremssdias@hotmail.com>
2022-04-28 23:45:46 +0200

    feat(food): Make one form to create the registered food and food consumed

--- apps/foods/forms/food_consumed_create_form.py
+++ apps/foods/forms/food_consumed_create_form.py
@@ -2,7 +2,7 @@ from django import forms
 from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 
-from apps.foods.models import FoodConsumed, RegisteredFood
+from apps.foods.models import FoodConsumed, RegisteredFood, Food
 
 
 class FoodConsumedCreateForm(forms.ModelForm):
@@ -12,12 +12,28 @@ class FoodConsumedCreateForm(forms.ModelForm):
         widgets = {
             "registered_food": forms.Select(attrs={"class": "form-control"}),
             "grams": forms.NumberInput(attrs={"class": "form-control"}),
-            "meal": forms.Select(attrs={"class": "form-control"})
+            "meal": forms.Select(attrs={"class": "form-control"}),
         }
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop("request")
         super().__init__(*args, **kwargs)
-        self.fields["registered_food"].queryset = RegisteredFood.objects.filter(
-            user_profile=self.request.user.profile
+        self.fields["registered_food"].queryset = Food.objects.filter(
+            Q(created_by=self.request.user.profile) | Q(created_by__isnull=True)
         )
+
+    def clean_registered_food(self):
+        user_profile = self.request.user.profile
+        food = self.cleaned_data.get("registered_food")
+        registered_food = RegisteredFood.objects.filter(
+            user_profile=user_profile, food=food
+        )
+
+        if registered_food.exists():
+            return registered_food.get()
+        else:
+            registered_food = RegisteredFood.objects.create(
+                user_profile=user_profile, food=food
+            )
+
+        return registered_food
