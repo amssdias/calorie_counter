8c1c2b29501379a1d27059fc716e38569f1090c2
André Dias <andremssdias@hotmail.com>
2022-06-10 15:25:03 +0200

    feat(accounts): Add tests to login form

--- apps/accounts/tests/forms/test_login_form.py
+++ apps/accounts/tests/forms/test_login_form.py
@@ -1,10 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
 
 from apps.accounts.forms import LoginForm
 from apps.accounts.models.user import User
 
 
-class TestLoginForm(TestCase):
+class LoginFormTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create_user(
@@ -14,25 +15,36 @@ class TestLoginForm(TestCase):
             "username": "testing@gmail.com",
             "password": "1234Testing",
         }
+        cls.form = LoginForm
+
+    def setUp(self):
+        self.form_data = {
+            "username": "test@testing.com",
+            "password": "1234Testing"
+        }
+
+    def test_login_form_email_label(self):
+        self.assertEqual(self.form().fields["username"].label, "Email")
 
     def test_form_valid(self):
-        data = {"username": "test@testing.com", "password": "1234Testing"}
-        login_form = LoginForm(data=data)
+        login_form = self.form(data=self.form_data)
         self.assertTrue(login_form.is_valid())
 
     def test_form_invalid_password(self):
-        data = {"username": "test@testing.com", "password": "Testing"}
-        login_form = LoginForm(data=data)
+        self.form_data["password"] = "wrong_password"
+        login_form = self.form(data=self.form_data)
         self.assertFalse(login_form.is_valid())
 
     def test_form_invalid_email(self):
-        data = {"username": "no_email@testing.com", "password": "testing123"}
-        login_form = LoginForm(data=data)
+        self.form_data["username"] = "no_email@testing.com"
+        login_form = self.form(data=self.form_data)
         self.assertFalse(login_form.is_valid())
 
     def test_form_invalid_inactive_user(self):
         self.user.is_active = False
         self.user.save()
-        data = {"username": "no_email@testing.com", "password": "testing123"}
-        login_form = LoginForm(data=data)
+        factory = RequestFactory()
+        request = factory.post(reverse("accounts:login"), data=self.form_data)
+        login_form = self.form(data=self.form_data)
+        login_form.request = request
         self.assertFalse(login_form.is_valid())
