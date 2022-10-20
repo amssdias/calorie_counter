5c5eb71b98b103754d0115b15070a96e9c20ebcf
André Dias <andremssdias@hotmail.com>
2022-10-21 00:03:42 +0200

    feat(accounts): change template to set password

--- apps/accounts/forms/set_password_form.py
+++ apps/accounts/forms/set_password_form.py
@@ -3,5 +3,5 @@ from django.contrib.auth.forms import SetPasswordForm
 class CustomSetPasswordForm(SetPasswordForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["new_password1"].widget.attrs.update({"class": "form__input margin-bottom-xsmall"})
-        self.fields["new_password2"].widget.attrs.update({"class": "form__input margin-bottom-xsmall"})
+        self.fields["new_password1"].widget.attrs.update({"class": "form__input form__input-text-align-center margin-bottom-xsmall"})
+        self.fields["new_password2"].widget.attrs.update({"class": "form__input form__input-text-align-center margin-bottom-xsmall"})
